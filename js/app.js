b9f347a691d13eac11c9e2bda37a4243acee1d5d
Shun Yu Rao <shunyu.rao@gmail.com>
2015-11-15 00:38:28 -0500

    Dialog system

--- js/app.js
+++ js/app.js
@@ -7,9 +7,15 @@
     });
 });
 
-var app = angular.module('lol', ['ui.router', 'ngVidBg', 'ngAudio', 'lol.services', 'lol.directives'])
+$(window).load(function () {
+    $("body").removeClass("preload");
+});
 
-.config(function ($stateProvider, $urlRouterProvider) {
+var app = angular.module('lol', ['ui.router', 'ngVidBg', 'ngAudio', 'ngAnimate', 'lol.services', 'lol.directives']);
+
+var ticks = 1000;
+
+app.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise("/title");
 
@@ -21,14 +27,11 @@ var app = angular.module('lol', ['ui.router', 'ngVidBg', 'ngAudio', 'lol.service
       })
 });
 
-app.controller('mainCtrl', function ($scope) {
-
-});
-
-app.controller('titleCtrl', function ($scope, ngAudio) {
+app.controller('titleCtrl', function ($scope, $timeout, ngAudio) {
 
     $scope.ambient = ngAudio.load("media/titleambient.mp3");
     $scope.ambient.volume = 0.3;
+    $scope.ambient.loop = true;
     $scope.ambient.play();
 
     $scope.bg = {
@@ -44,7 +47,46 @@ app.controller('titleCtrl', function ($scope, ngAudio) {
 
     $scope.start = function () {
         $scope.started = true;
-        $scope.ambient.stop();
+        $timeout(function () {
+            $scope.blur = true;
+            $timeout(function () {
+                $scope.storyTime = true;
+            }, 2 * ticks);
+        }, 1 * ticks);
+    }
+
+});
+
+app.controller('mainCtrl', function ($scope, $timeout, ngAudio) {
+
+    $scope.events = [];
+
+    $scope.options = [
+        { name: "Tell him to eat a dog", text: "Eat a dog!", isAction: false },
+        { name: "Jerk it", text: "You jerk it really hard", isAction: true },
+        { name: "Leave and never come back", text: "I AM LEAVING ", isAction: false }
+    ];
+
+    var addEvent = function (text, fromUser, isAction) {
+        $scope.events.push({
+            text: text, fromUser: fromUser, isAction: isAction
+        });
+    }
+
+    $scope.advance = function (option) {
+        addEvent(option.text, true, option.isAction);
+        $scope.options = [];
+        $timeout(function () {
+            $scope.options = [
+                { name: "Tasdasdog", text: "Eaasdadsd", isAction: false },
+                { name: "Jasdasdt", text: "Yasdasdasdy hard", isAction: true },
+                { name: "Leaasdde back", text: "I AMasdasdasdasdNG ", isAction: false }
+            ];
+        }, 1.5 * ticks);
+    }
+
+    $scope.getContentHeight = function () {
+        return $("#content").height();
     }
 
 });
